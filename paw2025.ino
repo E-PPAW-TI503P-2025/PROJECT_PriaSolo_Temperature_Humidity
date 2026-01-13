8c43193bc9073ae6a084b84cee6a20f32ca62e09
dzakyputra45 <dzakypratama45@gmail.com>
2026-01-13 12:32:37 +0700

    add the first project

---
+++ paw2025.ino
@@ -0,0 +1,43 @@
+#include "DHT.h"
+
+#define DHTPIN 4        // DATA DHT11
+#define DHTTYPE DHT11  // Tipe sensor
+#define BUZZER_PIN 19  // Buzzer
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float batasSuhu = 30.0; // Suhu batas buzzer (°C)
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  dht.begin();
+
+  Serial.println("=== Sistem Monitoring Suhu DHT11 ===");
+}
+
+void loop() {
+  float suhu = dht.readTemperature(); // Celsius
+
+  if (isnan(suhu)) {
+    Serial.println("GAGAL BACA SENSOR DHT!");
+    delay(3000);
+    return;
+  }
+
+  Serial.print("Suhu: ");
+  Serial.print(suhu);
+  Serial.println(" °C");
+
+  if (suhu >= batasSuhu) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    Serial.println("⚠️ Buzzer ON (Suhu Tinggi)");
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+
+  delay(3000); // DHT tidak boleh dibaca terlalu cepat
+}
